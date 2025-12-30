d1c6ab51d6d56a525ce79bf237079d6281cd7ef5
Selman KOYAN <resistablee7@hotmail.com>
2025-12-30 17:30:08 +0300

    Simplify RabbitMQ docker run command

--- RabbitMQ docker build.sh
+++ RabbitMQ docker build.sh
@@ -1,3 +1,1 @@
-docker pull rabbitmq
-
-docker run -d -p 1453:15672 -p 5672:5672 --name rabbitmq rabbitmq:3-management
+docker run -d --name rabbitmq -p 1453:15672 -p 5672:5672 rabbitmq:3-management
